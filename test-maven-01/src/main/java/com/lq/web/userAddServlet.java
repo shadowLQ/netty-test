443eeba9c01e5537e92cdbceb2471914d771714a
LQ <415804410@qq.com>
2019-04-13 19:49:08 +0800

    maven测试用例

---
+++ test-maven-01/src/main/java/com/lq/web/userAddServlet.java
@@ -0,0 +1,22 @@
+package com.lq.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author LQ
+ * @date 2019/04/13 17:07
+ */
+@javax.servlet.annotation.WebServlet(name = "userAddServlet")
+public class userAddServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ test-maven-01/src/main/webapp/index.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: LQ
+  Date: 2019/04/13
+  Time: 16:41
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <form>
+        用户名<input  type="text" name="lastName">
+        密码<input type="password" name="passWord">
+    </form>
+</body>
+</html>
